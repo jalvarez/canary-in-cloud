66fa60de6c6814dd047c7f2ce416bcf48428395e
Juan Miguel Alvarez <juanmi.alvarez@gmail.com>
2016-01-20 22:10:32 +0000

    Added logging in lambda_handlers

--- src/lambda_handlers.py
+++ src/lambda_handlers.py
@@ -1,9 +1,12 @@
 from AWS_context import AWSContext
+import logging
 
 def scan_handler(event, handler_context):
 	ctx = AWSContext()
 	for client in ctx.clients_repository.get_clients():
+		logging.info("Scanning urls of %s" % client['name'])
 		for client_url in ctx.clients_repository.get_client_urls(client['client_id']):
+			logging.info("Check url: %s" % client_url['url'])
 			canary = ctx.canary_factory.new(client_url['url'])
 			canary.check()
 			canary.register_response()
