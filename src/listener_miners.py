361d39d9fc2a0be477bc8204f66284cbdb3e1b70
Juan Miguel Alvarez <juanmi.alvarez@gmail.com>
2016-02-06 19:11:30 +0000

    Added test not alert when not changes in NotOkListenerMiner

--- src/listener_miners.py
+++ src/listener_miners.py
@@ -1,36 +1,47 @@
 class ListenerMiner:
-	def __init__(self, clients_repository, results_repository, client_id):
+	def __init__(self, 	clients_repository, results_repository, \
+						canary_factory, client_id):
 		self.clients_repository = clients_repository
 		self.results_repository = results_repository
+		self.canary_factory = canary_factory
 		self.client_id = client_id
 		self._is_alert = False
 
 	def listen_client_urls(self, listener):
 		for url in self.clients_repository.get_client_urls(self.client_id):
-			listener(url)
+			canary = self.canary_factory.new(url)
+			listener(canary, url)
 
 	def is_alert(self):
 		return self._is_alert
 
 class LastResultListenerMiner(ListenerMiner):
+	def is_result_ok(self, result):
+		return (result['status_code'] == 200)
+
 	def get_last_result(self, url):
 		results_serie = self.results_repository.resultsSerie_by_url(url)
 		return results_serie.last_result()
 
 class NotOkListenerMiner(LastResultListenerMiner):
-	def not_ok_listener(self, url):
+	def not_ok_listener(self, canary, url):
+		canary_check = canary.check()
 		last_result = self.get_last_result(url)
-		self._is_alert = (last_result['status_code'] != 200)
+		canary.register_response()
+		self._is_alert = self.is_result_ok(last_result) and \
+							not self.is_result_ok(canary_check)
 
 	def listen(self):
 		self.listen_client_urls(self.not_ok_listener)
 	
 class ListenerMinersFactory:
-	def __init__(self, clients_repository, results_repository):
+	def __init__(self, clients_repository, results_repository, canary_factory):
 		self.clients_repository = clients_repository
 		self.results_repository = results_repository
+		self.canary_factory = canary_factory
 
 	def new(self, minerClass, client_id):
 		return minerClass(self.clients_repository, \
 						  self.results_repository, \
+						  self.canary_factory, \
 						  client_id)
--- test/test_listener_miners.py
+++ test/test_listener_miners.py
@@ -6,12 +6,18 @@ import src
 class ListenerMinerTests(unittest.TestCase):
 	def setUp(self):
 		an_urls = [{ 'url': 'http://test.com' }]
+		self.client_id = 'TEST'
+		self.FAILED_RESULT = { 'status_code': 500 }
+		self.OK_RESULT = { 'status_code': 200 }
+
 		self.clients_repository_mock = Mock()
 		self.clients_repository_mock.get_client_urls = Mock(return_value=an_urls)
 		self.results_repository_mock = Mock()
+		self.canary_factory_mock = Mock()
 		self.miners_factory = src.ListenerMinersFactory( \
 												self.clients_repository_mock, \
-												self.results_repository_mock)
+												self.results_repository_mock, \
+												self.canary_factory_mock)
 	
 	def config_results_serie(self, return_result):
 		results_serie_mock = Mock()
@@ -19,12 +25,23 @@ class ListenerMinerTests(unittest.TestCase):
 		self.results_repository_mock.resultsSerie_by_url = Mock( \
 												return_value=results_serie_mock)
 
+	def config_canary(self, return_result):
+		canary_mock = Mock()
+		canary_mock.check = Mock(return_value=return_result)
+		self.canary_factory_mock.new = Mock(return_value=canary_mock)
+
 	def test_not_ok_listener_alert(self):
-		failed_result = { 'status_code': 500 }
-		self.config_results_serie(failed_result)
-		client_id = 'TEST'
+		self.config_canary(self.FAILED_RESULT)
+		self.config_results_serie(self.OK_RESULT)
 		not_ok_listener_miner = self.miners_factory.new(src.NotOkListenerMiner,\
-														client_id)
+														self.client_id)
 		not_ok_listener_miner.listen()
 		self.assertTrue(not_ok_listener_miner.is_alert())
 
+	def test_not_ok_listener_not_changes_not_alert(self):
+		self.config_canary(self.FAILED_RESULT)
+		self.config_results_serie(self.FAILED_RESULT)
+		not_ok_listener_miner = self.miners_factory.new(src.NotOkListenerMiner,\
+														self.client_id)
+		not_ok_listener_miner.listen()
+		self.assertFalse(not_ok_listener_miner.is_alert())
