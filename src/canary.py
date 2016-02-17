187808d5537360bd770ea5eb1454e8a8c19d2268
Juan Miguel Alvarez <juanmi.alvarez@gmail.com>
2016-02-17 11:38:44 +0000

    Change in canary.check to use a callback.

--- src/canary.py
+++ src/canary.py
@@ -3,44 +3,43 @@ import time
 import datetime
 
 class Canary:
-	def __init__(self, result_table, url):
-		self.result_table = result_table
-		self.url = url
+    def __init__(self, result_table, url):
+        self.result_table = result_table
+        self.url = url
 
-	def check(self):
-		self.result = {	'timestamp': str(time.time())
-						,'timestamp_iso': datetime.datetime.now().isoformat() 
-						,'status_code': 0
-						,'duration': 0
-				 		}
+    def check(self, check_callback):
+        self.result = { 'timestamp': str(time.time())
+                        ,'timestamp_iso': datetime.datetime.now().isoformat() 
+                        ,'status_code': 0
+                        ,'duration': 0
+                        }
 
-		try:	
-			start_time = time.clock()
-			response = urllib.urlopen(self.url)
-			self.result['status_code'] = response.getcode()
-			end_time = time.clock()
-			self.result['duration'] = int((end_time - start_time) * 1000)
-		except IOError:
-			self.result['status_code'] = 404 # Not found
-		except:
-			self.result['status_code'] = 418 # I'm a teapot (RFC 2324)
-		finally:
-			try:
-				response.close()
-			except:
-				None
+        try:    
+            start_time = time.clock()
+            response = urllib.urlopen(self.url)
+            self.result['status_code'] = response.getcode()
+            end_time = time.clock()
+            self.result['duration'] = int((end_time - start_time) * 1000)
+        except IOError:
+            self.result['status_code'] = 404 # Not found
+        except:
+            self.result['status_code'] = 418 # I'm a teapot (RFC 2324)
+        finally:
+            try:
+                response.close()
+            except:
+                None
+            check_callback(self.result)
 
-		return self.result
-
-	def register_response(self):
-		if not hasattr(self, 'result'):
-			raise RegisterWithoutCheckError()
-		self.result_table.put_item(Item={ 'url': self.url
-										,'time': self.result['timestamp']
-										,'timestamp_iso': self.result['timestamp_iso']
-										,'status_code': self.result['status_code']
-										,'response_ms': self.result['duration']
-										})
+    def register_response(self):
+        if not hasattr(self, 'result'):
+            raise RegisterWithoutCheckError()
+        self.result_table.put_item(Item={ 'url': self.url
+                                        ,'time': self.result['timestamp']
+                                        ,'timestamp_iso': self.result['timestamp_iso']
+                                        ,'status_code': self.result['status_code']
+                                        ,'response_ms': self.result['duration']
+                                        })
 
 class RegisterWithoutCheckError(Exception):
-	pass
+    pass
--- test/test_canary.py
+++ test/test_canary.py
@@ -1,48 +1,57 @@
 import unittest
 import types
+from functools import partial
 from mock import Mock
 
 import src
 
 class CheckUrlTests(unittest.TestCase):
-	def setUp(self):
-		self.result_table_mock = Mock()
+    def setUp(self):
+        self.result_table_mock = Mock()
 
-	def aNewCanary(self, url):
-		return src.Canary(self.result_table_mock, url)
+    def aNewCanary(self, url):
+        return src.Canary(self.result_table_mock, url)
 
-	def test_wrong_url(self):
-		canary = self.aNewCanary('http://estaurlseguroquenoexiste.com')
-		result = canary.check()
-		self.assertEqual(404, result['status_code'])
+    def check_callback(self, assert_method, result_func, expected, result):
+        assert_method(result_func(result), expected)
 
-	def test_ok_url(self):
-		canary = self.aNewCanary('http://www.google.com')
-		result = canary.check()
-		self.assertEqual(200, result['status_code'])
+    def in_canary_check(self, canary, \
+                        assert_method, result_func, expected=None):
+        canary.check(partial(self.check_callback, \
+                             assert_method, result_func, expected))
 
-	def test_ok_duration_measured(self):
-		canary = self.aNewCanary('http://www.google.com')
-		result = canary.check()
-		self.assertGreater(result['duration'], 0)
+    def test_wrong_url(self):
+        canary = self.aNewCanary('http://estaurlseguroquenoexiste.com')
+        self.in_canary_check(canary, \
+                        self.assertEqual, lambda x: x['status_code'], 404)
 
-	def test_ok_with_timestamp(self):
-		canary = self.aNewCanary('http://www.google.com')
-		result = canary.check()
-		self.assertIsNotNone(result['timestamp'])
+    def test_ok_url(self):
+        canary = self.aNewCanary('http://www.google.com')
+        self.in_canary_check(canary, \
+                        self.assertEqual, lambda x: x['status_code'], 200)
 
-	def test_timestamp_is_string(self):
-		canary = self.aNewCanary('http://www.google.com')
-		result = canary.check()
-		timestamp = result['timestamp']
-		self.assertIn(type(timestamp), types.StringTypes)
+    def test_ok_duration_measured(self):
+        canary = self.aNewCanary('http://www.google.com')
+        self.in_canary_check(canary, \
+                        self.assertGreater, lambda x: x['duration'], 0)
 
-	def test_duration_is_integer(self):
-		canary = self.aNewCanary('http://www.google.com')
-		result = canary.check()
-		duration = result['duration']
-		self.assertEquals(type(duration), types.IntType)
+    def test_ok_with_timestamp(self):
+        canary = self.aNewCanary('http://www.google.com')
+        self.in_canary_check(canary, \
+                        self.assertIsNotNone, lambda x: x['timestamp'])
 
-	def test_not_register_without_check(self):
-		canary = self.aNewCanary('http://www.google.com')
-		self.assertRaises(src.RegisterWithoutCheckError, canary.register_response)
+    def test_timestamp_is_string(self):
+        canary = self.aNewCanary('http://www.google.com')
+        self.in_canary_check(canary, \
+                        self.assertIn, lambda x: type(x['timestamp']), \
+                        types.StringTypes)
+
+    def test_duration_is_integer(self):
+        canary = self.aNewCanary('http://www.google.com')
+        self.in_canary_check(canary, \
+                        self.assertEquals, lambda x: type(x['duration']), \
+                        types.IntType)
+
+    def test_not_register_without_check(self):
+        canary = self.aNewCanary('http://www.google.com')
+        self.assertRaises(src.RegisterWithoutCheckError, canary.register_response)
