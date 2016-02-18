0cc1f316bb865035a2ad481c637366fc7b77c707
Juan Miguel Alvarez <juanmi.alvarez@gmail.com>
2016-02-18 08:43:54 +0000

    Test canary with HTTPS urls.

--- test/test_canary.py
+++ test/test_canary.py
@@ -23,40 +23,40 @@ class CheckUrlTests(unittest.TestCase):
                              assert_method, result_func, expected))
 
     def test_wrong_url(self):
-        canary = self.aNewCanary('http://estaurlseguroquenoexiste.com')
+        canary = self.aNewCanary('https://estaurlseguroquenoexiste.com')
         return self.in_canary_check(canary, \
                                   self.assertEqual, lambda x: x['status_code'],\
                                   404)
 
     def test_ok_url(self):
-        canary = self.aNewCanary('http://www.google.com')
+        canary = self.aNewCanary('https://www.google.com')
         return self.in_canary_check(canary, \
                                     self.assertIn, lambda x: x['status_code'],\
                                     [200, 302])
 
     def test_ok_duration_measured(self):
-        canary = self.aNewCanary('http://www.google.com')
+        canary = self.aNewCanary('https://www.google.com')
         return self.in_canary_check(canary, \
                                 self.assertGreater, lambda x: x['duration'], 0)
 
     def test_ok_with_timestamp(self):
-        canary = self.aNewCanary('http://www.google.com')
+        canary = self.aNewCanary('https://www.google.com')
         return self.in_canary_check(canary, \
                                 self.assertIsNotNone, lambda x: x['timestamp'])
 
     def test_timestamp_is_string(self):
-        canary = self.aNewCanary('http://www.google.com')
+        canary = self.aNewCanary('https://www.google.com')
         return self.in_canary_check(canary, \
                                 self.assertIn, lambda x: type(x['timestamp']), \
                                 types.StringTypes)
 
     def test_duration_is_integer(self):
-        canary = self.aNewCanary('http://www.google.com')
+        canary = self.aNewCanary('https://www.google.com')
         return self.in_canary_check(canary, \
                             self.assertEquals, lambda x: type(x['duration']), \
                             types.IntType)
 
     def test_not_register_without_check(self):
-        canary = self.aNewCanary('http://www.google.com')
+        canary = self.aNewCanary('https://www.google.com')
         self.assertRaises(src.RegisterWithoutCheckError, \
                           canary.register_response)
